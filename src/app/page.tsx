13a501b314a00e55d15565ccc5effda32d26b8d4
Italo Vinicius <italoviniciusrope@gmail.com>
2023-05-16 23:45:54 -0300

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           <p className="w-[360px] text-center leading-relaxed">
             Você ainda não registrou nenhuma lembrança, comece a{` `}{" "}
             <a href="" className="underline hover:text-gray-50">
-              criar agora!
+              criar agora!!!
             </a>
           </p>
         </div>
