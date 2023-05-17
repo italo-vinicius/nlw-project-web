4037309e18145608103c857927bcdb9b5571d82e
Italo Vinicius <italoviniciusrope@gmail.com>
2023-05-17 14:04:04 -0300

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         sans: "var(--font-roboto)",
         alt: "var(--font--bai-jamjuree)",
       },
+      fontSize: {
+        "5xl": "2.5rem",
+      },
       colors: {
         gray: {
           50: "#eaeaea",
@@ -72,6 +75,13 @@ module.exports = {
           800: "#027435",
           900: "#025929",
         },
+      },
+      backgroundImage: {
+        stripes:
+          "linear-gradient(to bottom, rgba(255, 255, 255, 0.1 ), rgba(255, 255, 255, 0.1 ) 12.5%, transparent 12.5%, transparent)",
+      },
+      backgroundSize: {
+        stripes: "100% 9px",
       },
       blur: { full: "194px" },
     },
