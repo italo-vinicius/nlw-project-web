31d7df4d79b609861e89dcee620d446613c2c999
Italo Vinicius <italoviniciusrope@gmail.com>
2023-05-18 19:15:37 -0300

    wip

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 pelo{" "}
+      <a
+        href="https://it4lodev.vercel.app/"
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-50"
+      >
+        Italo do back
+      </a>
+    </div>
+  );
+}
